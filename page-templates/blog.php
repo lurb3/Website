0edafcd884721731f66364ae4f3ed2508bb25c73
unknown <gustavo.gigante.s@gmail.com>
2019-01-29 23:14:14 +0000

    new v

---
+++ page-templates/blog.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Template Name: Blog Layout
+ *
+ * This template can be used to override the default template and sidebar setup
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php
+	$post_id = 1;
+	$queried_post = get_post($post_id);
+	$title = $queried_post->post_title;
+?>
+
+<div id="blog">
+
+	<div class="container">
+		<div class="row mb-5 mt-5">
+			<div class="col-12 text-center">
+				<h2 class="text-light">Latest Posts</h2>
+			</div>
+		</div>
+		<div class="row">
+				<?php 
+					$args = array( 'numberposts' => -1); 
+					$posts= get_posts( $args );
+					$count = 0;
+					if ($posts) {
+						foreach ( $posts as $post ) {
+							echo "<div class='col-6 text-dark text-center p-3'>";
+								echo "<div class='bg-light p-2'>";
+									setup_postdata($post);
+									the_title();
+									the_content();
+									the_excerpt();
+								echo "</div>";
+							echo "</div>";
+						}
+					} 
+				?>
+		</div>
+	</div>	
+
+</div>
+
+<?php get_footer(); ?>
