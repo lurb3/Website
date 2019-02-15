d1e54003d965b1e4966171de26e65fb391dc03b0
unknown <gustavo.gigante.s@gmail.com>
2019-02-15 23:16:55 +0000

    About Page Design

--- page-templates/sobre.php
+++ page-templates/sobre.php
@@ -15,25 +15,34 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div id="sobre">
+<div id="about">
 
-	<div class="container">
+	<div class="container about-container">
 		<?php
 			include get_template_directory() . '/inc/nav-menu.php';
 		?> <!-- Include Nav Menu --> <!-- Include Nav Menu -->
-		<div class="row bg-light p-3 text-center sobre-content">
-			<div class="thumbnail m-auto pb-3">
-				<?php 
+		<div class="row p-3 text-center about-content">
+			<div class="col-6">
+				<div class="thumbnail m-auto pb-3">
+					<?php 
+						if ( have_posts() ) : while ( have_posts() ) : the_post();
+							the_post_thumbnail(); 
+						endwhile; endif;
+					?>
+					<div class="mt-4">
+						<h2 class="d-inline">That's Me</h2>
+						<i class="fas fa-long-arrow-alt-up"></i>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 about-text">
+				<h2 class="text-left">Sobre</h2>
+				<?php
 					if ( have_posts() ) : while ( have_posts() ) : the_post();
-						the_post_thumbnail(); 
-					endwhile; endif;
+						the_content();
+					endwhile; endif; 
 				?>
 			</div>
-			<?php
-				if ( have_posts() ) : while ( have_posts() ) : the_post();
-					the_content();
-				endwhile; endif; 
-			?>
 		</div>
 	</div>	
 
