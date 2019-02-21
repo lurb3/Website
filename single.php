c14f08a6bfde41252138fab8dd83f316dd6ecca6
unknown <gustavo.gigante.s@gmail.com>
2019-02-21 22:19:37 +0000

    Post Page Restructure

--- single.php
+++ single.php
@@ -13,41 +13,20 @@ get_header();
 $container   = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<div class="wrapper" id="single-wrapper">
+<div class="container">
+	<?php
+		include get_template_directory() . '/inc/nav-menu.php';
+	?>
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+	<div class="row">
+		<?php while ( have_posts() ) : the_post(); ?>
 
-		<div class="row">
+			<?php get_template_part( 'loop-templates/content', 'single' ); ?>
+			<?php understrap_post_nav(); ?>
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+		<?php endwhile; // end of the loop. ?>
+		<?php get_footer(); ?>
+	</div>
 
-			<main class="site-main" id="main">
+</div>
 
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
-
-						<?php understrap_post_nav(); ?>
-
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
-
-				<?php endwhile; // end of the loop. ?>
-
-			</main><!-- #main -->
-
-		<!-- Do the right sidebar check -->
-		<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
-
-	</div><!-- .row -->
-
-</div><!-- Container end -->
-
-</div><!-- Wrapper end -->
-
-<?php get_footer(); ?>
