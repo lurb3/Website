e84d797278c8dd9035fe943a55901ca34498b94e
unknown <gustavo.gigante.s@gmail.com>
2019-03-06 22:46:01 +0000

    Changes to Blog Page and Post Pages

--- single.php
+++ single.php
@@ -12,22 +12,60 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 $container   = get_theme_mod( 'understrap_container_type' );
 ?>
-
 <div class="container">
 	<?php
 		include get_template_directory() . '/inc/nav-menu.php';
 	?>
+</div>
+
+<div class="container mw-100 m-0">
+	<?php
+		$post_thumbnail = get_the_post_thumbnail_url();
+		$post_title = get_the_title();
+		$post_date = get_the_date();
+		$modified_date = get_the_modified_date();
+		echo "
+			<div
+				class='row post-image'
+				style='background-image:url(\"" . $post_thumbnail . "\"); background-position:center; background-size:cover; height:500px;'
+			>
+				<div class='m-auto text-center' style='z-index:999;'>
+					<h1 class='text-light'>
+						" . $post_title . "
+					</h1>
+					<p class='text-light'>
+						Publish Date: " . $post_date . "
+					</p>
+					<p class='text-light'>
+						Last Update: " . $modified_date . "
+					</p>
+				</div>
+			</div>
+		";
+	?>
+</div>
+
+<div class="container mt-3">
 
 	<div class="row">
-		
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'loop-templates/content', 'single' ); ?>
-			<?php understrap_post_nav(); ?>
-
-		<?php endwhile; // end of the loop. ?>
-		<?php get_footer(); ?>
+		<?php
+			$post_ID = get_the_ID();
+			$content_post = get_post($post_ID);
+			$content = $content_post -> post_content;
+			$previous_post = get_previous_post();
+			echo "
+				<div class='pl-5 pr-5'>
+					<p>" . $content . "</p>
+					<p>
+						<a href='" . get_permalink( $previous_post->ID ) . "'>
+							<- ".$previous_post -> post_title ."
+						</a>
+					</p>
+				</div>
+			";
+		?>
 	</div>
 
 </div>
 
+<?php get_footer(); ?>
