2f18f989080b409f9aaf653c303d37015b4a60fa
Gustavo <gustavo.gigante.s@gmail.com>
2019-06-16 21:54:43 +0100

    Added card's bg image + restyle + links

--- page-templates/projects.php
+++ page-templates/projects.php
@@ -33,7 +33,6 @@ $container = get_theme_mod( 'understrap_container_type' );
             /*
                 TODO: Put Array into backoffice
                 TODO: Button bellow?
-                TODO: &:hover - show background image
                 TODO: Add Links to each card
                 TODO: Seperate cards through categories
             */
@@ -42,50 +41,59 @@ $container = get_theme_mod( 'understrap_container_type' );
                         'Url' => '',
                         'Title' => 'BudgetApp',
                         'SubTitle' => 'Web App to control personal spendings',
-                        'TimeSpent' => '1 month (WIP)'
+                        'TimeSpent' => '1 month (WIP)',
+                        'Image' => 'budget-app.jpg'
                     ],
                     'GustavoMonteiro' => [
-                        'Url' => '',
+                        'Url' => 'https://gustavomonteiro.pt/',
                         'Title' => 'Gustavo Monteiro Website',
                         'SubTitle' => 'Personal website + blog',
-                        'TimeSpent' => '2 weeks (WIP)'
+                        'TimeSpent' => '2 weeks (WIP)',
+                        'Image' => 'gustavo-website.jpg'
                     ],
                     'Tendoledu' => [
-                        'Url' => '',
+                        'Url' => 'https://tendoledu.com/students/',
                         'Title' => 'Tendoledu',
                         'SubTitle' => 'Landing Page for School Business',
-                        'TimeSpent' => '2 days'
+                        'TimeSpent' => '2 days',
+                        'Image' => 'tenduledo-project.jpg'
                     ],
                     'AnaPeneda' => [
-                        'Url' => '',
+                        'Url' => 'https://gustavomonteiro.pt/ana-website/',
                         'Title' => 'Ana Peneda Website',
                         'SubTitle' => 'Website to promote personal brand',
-                        'TimeSpent' => '1 week (WIP)'
+                        'TimeSpent' => '1 week (WIP)',
+                        'Image' => 'ana-website.jpg'
                     ],
                     'CounterApp' => [
                         'Url' => '',
                         'Title' => 'Counter App',
                         'SubTitle' => 'Web App to count time spent coding',
-                        'TimeSpent' => '0 days (WIP)'
+                        'TimeSpent' => '0 days (WIP)',
+                        'Image' => ''
                     ],
                     'Dashboard' => [
                         'Url' => '',
                         'Title' => 'Dashboard',
                         'SubTitle' => 'Dashboard template for public use',
-                        'TimeSpent' => '0 days (WIP)'
+                        'TimeSpent' => '0 days (WIP)',
+                        'Image' => ''
                     ]
                 ];
 
                 foreach($cardsArray as $card) {
+                    $templateURI = get_bloginfo('template_directory');
                     echo "
                         <div class='col-lg-12'>
-                            <div class='projectsCard'>
-                                <div class='cardText'>
-                                    <h2>" . $card['Title'] . "</h2>
-                                    <span>" . $card['SubTitle'] . "</span>
-                                    <span>Time Spent: " . $card['TimeSpent'] . "</span>
+                            <a href='" . $card['Url'] . "' target='_blank'>
+                                <div class='projectsCard' style='background-image: url(" . $templateURI . "/images/" . $card['Image']. ");'>
+                                    <div class='cardText'>
+                                        <h2>" . $card['Title'] . "</h2>
+                                        <span>" . $card['SubTitle'] . "</span>
+                                        <span>Time Spent: " . $card['TimeSpent'] . "</span>
+                                    </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
                     ";
                 }
