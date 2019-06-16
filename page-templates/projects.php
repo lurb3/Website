af8b08c81befe340d2dfa475d1469f1e620f5c8e
Gustavo <gustavo.gigante.s@gmail.com>
2019-06-16 19:25:43 +0100

    Changes to Cards styling

--- page-templates/projects.php
+++ page-templates/projects.php
@@ -29,9 +29,29 @@ $container = get_theme_mod( 'understrap_container_type' );
             </div>
         </div>
         <div class="row">
+            <!-- TODO: Add Links to each card -->
             <!-- TODO: create a for loop for each card -->
-            <div class="col-lg-4">
-                <div class="projectsCard" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/tenduledo-project.jpg);'">
+            <!-- TODO: Seperate cards through categories -->
+            <div class="col-lg-6">
+                <div class="projectsCard" alt="Web App - Budget App" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/budget-app.jpg);'">
+                    <div class="cardText">
+                        <h2>BudgetApp</h2>
+                        <span>Web App to control personal spendings</span>
+                        <span>Time Spent: 1 month (WIP)</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="projectsCard" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/gustavo-website.jpg);'">
+                    <div class="cardText">
+                        <h2>Gustavo Monteiro Website</h2>
+                        <span>Personal website + blog</span>
+                        <span>Time Spent: 2 weeks (WIP)</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="projectsCard" alt="Tenduledo Landing Page" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/tenduledo-project.jpg);'">
                     <div class="cardText">
                         <h2>Tendoledu</h2>
                         <span>Landing Page for School Business</span>
@@ -39,48 +59,30 @@ $container = get_theme_mod( 'understrap_container_type' );
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
-                <div class="projectsCard" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/tenduledo-project.jpg);'">
+            <div class="col-lg-6">
+                <div class="projectsCard" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/ana-website.jpg);'">
                     <div class="cardText">
-                        <h2>Tendoledu</h2>
-                        <span>Landing Page for School Business</span>
-                        <span>Time Spent: 2 days</span>
+                        <h2>Ana Peneda Website</h2>
+                        <span>Website to promote personal brand</span>
+                        <span>Time Spent: 1 weeks (WIP)</span>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <div class="projectsCard" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/tenduledo-project.jpg);'">
                     <div class="cardText">
-                        <h2>Tendoledu</h2>
-                        <span>Landing Page for School Business</span>
-                        <span>Time Spent: 2 days</span>
+                        <h2>Counter App</h2>
+                        <span>Web App to count time spent coding</span>
+                        <span>Time Spent: 0 days (WIP)</span>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <div class="projectsCard" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/tenduledo-project.jpg);'">
                     <div class="cardText">
-                        <h2>Tendoledu</h2>
-                        <span>Landing Page for School Business</span>
-                        <span>Time Spent: 2 days</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="projectsCard" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/tenduledo-project.jpg);'">
-                    <div class="cardText">
-                        <h2>Tendoledu</h2>
-                        <span>Landing Page for School Business</span>
-                        <span>Time Spent: 2 days</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="projectsCard" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/tenduledo-project.jpg);'">
-                    <div class="cardText">
-                        <h2>Tendoledu</h2>
-                        <span>Landing Page for School Business</span>
-                        <span>Time Spent: 2 days</span>
+                        <h2>Dashboard</h2>
+                        <span>Dashboard template for public use</span>
+                        <span>Time Spent: 0 days (WIP)</span>
                     </div>
                 </div>
             </div>
